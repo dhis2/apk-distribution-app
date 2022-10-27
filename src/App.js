e9257541dcab669c479c3673a20e8733f1e77034
Milagros <sharmynrodriguez@gmail.com>
2022-10-27 11:11:35 +0200

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { CenteredContent, CssVariables, CssReset} from '@dhis2/ui'
 import React from 'react'
 import classes from './App.module.css'
-import {ApkList} from "./pages";
+import {ApkList, UploadApk} from "./pages";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+
+const RouterP = () => (
+    <Routes>
+        <Route exact path="/" element={<ApkList/>} />
+        <Route path="/new" element={<UploadApk/>} />
+        {/*No-match route - redirect to index*/}
+        <Route render={() => <Navigate to="/" replace/>} />
+    </Routes>
+)
 
 const MyApp = () => (
     <>
-        <CssReset />
-        <CssVariables colors spacers />
-        <div className={classes.container}>
-            <CenteredContent>
-                <ApkList/>
-            </CenteredContent>
-        </div>
+        <HashRouter>
+            <CssReset />
+            <CssVariables colors spacers />
+            <div className={classes.container}>
+                <CenteredContent>
+                    <RouterP/>
+                </CenteredContent>
+            </div>
+        </HashRouter>
     </>
 )
 
