d99e92b7b1d6c2f190609710391ada2840cd259f
Milagros <sharmynrodriguez@gmail.com>
2023-03-27 15:22:50 +0200

    fix: remove unused react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import { CenteredContent, CssVariables, CssReset } from '@dhis2/ui'
 import React from 'react'
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import classes from './App.module.css'
 import { ApkList } from './pages'
 
-const RouterP = () => (
-    <Routes>
-        <Route exact path="/" element={<ApkList />} />
-        <Route render={() => <Navigate to="/" replace />} />
-    </Routes>
-)
-
-const MyApp = () => (
+const App = () => (
     <>
-        <HashRouter>
-            <CssReset />
-            <CssVariables colors spacers />
-            <div className={classes.container}>
-                <CenteredContent>
-                    <RouterP />
-                </CenteredContent>
-            </div>
-        </HashRouter>
+        <CssReset />
+        <CssVariables colors spacers />
+        <div className={classes.container}>
+            <CenteredContent>
+                <ApkList />
+            </CenteredContent>
+        </div>
     </>
 )
 
-export default MyApp
+export default App
