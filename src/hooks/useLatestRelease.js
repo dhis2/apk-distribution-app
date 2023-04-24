06a2e7f4786fff8769d3d291d5b34b30ae313cef
Milagros <sharmynrodriguez@gmail.com>
2023-04-24 11:58:23 +0200

    feat: get latest gitHub release [DHIS2-14963]

---
+++ src/hooks/useLatestRelease.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+export const useLatestRelease = () => {
+    const githubURL =
+        'https://api.github.com/repos/dhis2/dhis2-android-capture-app/releases/latest'
+    const [release, setRelease] = useState(null)
+
+    useEffect(() => {
+        const fetchRelease = async () => {
+            const response = await fetch(githubURL)
+            const data = await response.json()
+            setRelease({
+                downloadURL: data?.assets[0].browser_download_url,
+                version: data?.tag_name,
+            })
+        }
+
+        fetchRelease()
+    }, [])
+
+    return release
+}
