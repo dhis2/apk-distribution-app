454f4ad25e04fc8e3786a2a7125ec55f02aa0f0e
Milagros <sharmynrodriguez@gmail.com>
2023-04-24 12:04:23 +0200

    feat: create first time setup component [DHIS2-14965]

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from './AppIcon/AppIcon'
 export * from './DownloadButton/DownloadButton'
+export * from './FirstTimeSetup/FirstTimeSetup'
 export * from './Page/Page'
 export * from './VersionTable/VersionTable'
