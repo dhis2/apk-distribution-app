70c73a3713b3d467cd33b106339d31071f2906ad
Milagros <sharmynrodriguez@gmail.com>
2023-05-07 20:11:45 +0200

    feat: no auth mode disables elements [DHIS2-14964]

--- src/pages/ApkList/ApkList.js
+++ src/pages/ApkList/ApkList.js
@@ -3,12 +3,14 @@ import { Card, Divider, CircularLoader } from '@dhis2/ui'
 import classnames from 'classnames'
 import isEmpty from 'lodash/isEmpty'
 import React, { useState, useEffect } from 'react'
+import { useIsAuthorized } from '../../auth'
 import { VersionTable } from '../../components'
 import { useDataStore } from '../../hooks'
 import styles from './ApkList.module.css'
 import { AboutSection, HeaderContent } from './Sections'
 
 export const ApkList = () => {
+    const { hasAuthority } = useIsAuthorized()
     const { loading, latestVersion, versions } = useDataStore()
     const [apkList, setList] = useState([])
     const [currentVersion, setVersion] = useState({})
@@ -31,6 +33,7 @@ export const ApkList = () => {
                         updateVersion={setVersion}
                         versions={apkList}
                         handleList={setList}
+                        disabled={!hasAuthority}
                     />
                     {loading ? (
                         <CircularLoader />
@@ -49,7 +52,10 @@ export const ApkList = () => {
                                             'All versions of the application'
                                         )}
                                     </h2>
-                                    <VersionTable versions={apkList} />
+                                    <VersionTable
+                                        versions={apkList}
+                                        disabled={!hasAuthority}
+                                    />
                                 </>
                             )}
                         </>
