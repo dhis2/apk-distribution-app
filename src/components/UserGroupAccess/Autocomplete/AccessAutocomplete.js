4d419c6c247878a8bb335bf044b3c5c4fc3b361a
Milagros <sharmynrodriguez@gmail.com>
2024-04-03 10:58:41 -0500

    feat: create basic components to search user groups

---
+++ src/components/UserGroupAccess/Autocomplete/AccessAutocomplete.js
@@ -0,0 +1,79 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import PropTypes from 'prop-types'
+import React, { useState, useEffect, useCallback } from 'react'
+import { debounce } from '../helper'
+import { Autocomplete } from './Autocomplete'
+
+const query = {
+    search: {
+        resource: 'userGroups',
+        params: ({ search }) => ({
+            query: search,
+        }),
+    },
+}
+
+export const AccessAutocomplete = ({ selected, onSelection }) => {
+    const [search, setSearch] = useState('')
+    const [showResults, setShowResults] = useState(false)
+    const { data, refetch, fetching } = useDataQuery(query, {
+        lazy: true,
+        onComplete: () => setShowResults(true),
+    })
+
+    /**
+     * On selection this syncs the displayName of the parent selection to the local input state.
+     */
+
+    useEffect(() => setSearch(selected), [selected])
+
+    /**
+     * If the search string changes and is truthy, send out a request, otherwise
+     * clear the selection.
+     */
+
+    const debouncedRefetch = useCallback(debounce(refetch, 250), [refetch])
+
+    useEffect(() => {
+        if (search && search === selected) {
+            return
+        }
+
+        if (search) {
+            debouncedRefetch({ search })
+        } else {
+            onSelection(null)
+            setShowResults(false)
+        }
+    }, [search])
+
+    // Concatenate all the results
+    let results = []
+
+    if (data?.search?.userGroups) {
+        results = data?.search?.userGroups
+    }
+
+    return (
+        <Autocomplete
+            inputWidth="400px"
+            label={i18n.t('User Group')}
+            loading={fetching}
+            placeholder={i18n.t('Search')}
+            search={search}
+            searchResults={showResults ? results : []}
+            onClose={() => setShowResults(false)}
+            onSearch={setSearch}
+            onSelect={(id) => {
+                onSelection(results.find((result) => result.id === id))
+                setShowResults(false)
+            }}
+        />
+    )
+}
+
+AccessAutocomplete.propTypes = {
+    onSelection: PropTypes.func.isRequired,
+    selected: PropTypes.string,
+}
---
+++ src/components/UserGroupAccess/Autocomplete/Autocomplete.js
@@ -0,0 +1,77 @@
+import { InputField, Menu, MenuItem } from '@dhis2/ui'
+import useSize from '@react-hook/size'
+import PropTypes from 'prop-types'
+import React, { useRef } from 'react'
+import { MenuPopup } from './MenuPopup'
+
+export const Autocomplete = ({
+    dataTest,
+    inputWidth,
+    label,
+    loading,
+    onClose,
+    onSearch,
+    onSelect,
+    placeholder,
+    search,
+    searchResults,
+}) => {
+    const wrapper = useRef(null)
+    const [menuWidth] = useSize(wrapper)
+
+    return (
+        <>
+            <div ref={wrapper}>
+                <InputField
+                    label={label}
+                    loading={loading}
+                    placeholder={placeholder}
+                    onChange={({ value }) => onSearch(value)}
+                    value={search}
+                    inputWidth={inputWidth}
+                />
+            </div>
+            {searchResults.length > 0 && (
+                <MenuPopup
+                    onClick={onClose}
+                    menuWidth={`${menuWidth}px`}
+                    menuRef={wrapper}
+                    dataTest={`${dataTest}-menu`}
+                >
+                    <Menu>
+                        {searchResults.map((result) => (
+                            <MenuItem
+                                key={result.id}
+                                label={result.displayName}
+                                value={result.id}
+                                onClick={({ value }) => onSelect(value)}
+                            />
+                        ))}
+                    </Menu>
+                </MenuPopup>
+            )}
+        </>
+    )
+}
+
+Autocomplete.defaultProps = {
+    dataTest: 'dhis2-sharingdialog-autocomplete',
+}
+
+Autocomplete.propTypes = {
+    searchResults: PropTypes.arrayOf(
+        PropTypes.shape({
+            displayName: PropTypes.string,
+            id: PropTypes.string,
+        })
+    ).isRequired,
+    dataTest: PropTypes.string,
+    inputWidth: PropTypes.string,
+    label: PropTypes.string,
+    loading: PropTypes.bool,
+    placeholder: PropTypes.string,
+    search: PropTypes.string,
+    onClose: PropTypes.func,
+    onSearch: PropTypes.func,
+    onSelect: PropTypes.func,
+}
